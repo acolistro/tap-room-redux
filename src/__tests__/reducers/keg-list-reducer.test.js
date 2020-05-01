47fd05591c1e0273540c70fc2a2ccc6d941cc99c
acolistro <arcolistro@gmail.com>
2020-05-01 10:09:26 -0700

    add delete keg test, debugging and add code to pass test.

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,26 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  
+  const currentState = {
+    1: { beer: "Bitburger",
+    dateTapped: "1/2/20",
+    brand: "Bitburger",
+    style: "Pilsener",
+    pricePerPint: "$8.00",
+    alcoholContent: "6.0%",
+    pintsLeft: 100,
+    id: 1 },
+    2: { beer: "Andechs",
+    dateTapped: "3/2/20",
+    brand: "Andechs",
+    style: "Helles",
+    pricePerPint: "$9.00",
+    alcoholContent: "6.3%",
+    pintsLeft: 120,
+    id: 2 }
+  }
+  
   let action;
   const kegData = {
     beer: "Bitburger",
@@ -10,7 +30,7 @@ describe('kegListReducer', () => {
     pricePerPint: "$8.00",
     alcoholContent: "6.0%",
     pintsLeft: 100,
-    id: 1,
+    id: 1
   };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -42,6 +62,23 @@ describe('kegListReducer', () => {
         pintsLeft: pintsLeft,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: { beer: "Andechs",
+      dateTapped: "3/2/20",
+      brand: "Andechs",
+      style: "Helles",
+      pricePerPint: "$9.00",
+      alcoholContent: "6.3%",
+      pintsLeft: 120,
+      id: 2 }
     });
   });
 
