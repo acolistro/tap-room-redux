3bd2a221973759273d6c450e0f91bbfe594e64a9
acolistro <arcolistro@gmail.com>
2020-05-01 09:02:23 -0700

    update keg list reducer and pass second test.

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,48 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    beer: "Bitburger",
+    dateTapped: "1/2/20",
+    brand: "Bitburger",
+    style: "Pilsener",
+    pricePerPint: "$8.00",
+    alcoholContent: "6.0%",
+    pintsLeft: 100,
+    id: 1,
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { beer, dateTapped, brand, style, pricePerPint, alcoholContent, pintsLeft, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      beer: beer,
+      dateTapped: dateTapped,
+      brand: brand,
+      style: style,
+      pricePerPint: pricePerPint,
+      alcoholContent: alcoholContent,
+      pintsLeft: pintsLeft,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        beer: beer,
+        dateTapped: dateTapped,
+        brand: brand,
+        style: style,
+        pricePerPint: pricePerPint,
+        alcoholContent: alcoholContent,
+        pintsLeft: pintsLeft,
+        id: id
+      }
+    });
+  });
+
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,20 @@
 export default (state = {}, action) => {
-
+  switch (action.type) {
+    case 'ADD_KEG':
+      const { beer, dateTapped, brand, style, pricePerPint, alcoholContent, pintsLeft, id  } = action;
+      return Object.assign({}, state, {
+        [id]: {
+          beer: beer,
+          dateTapped: dateTapped,
+          brand: brand,
+          style: style,
+          pricePerPint: pricePerPint,
+          alcoholContent: alcoholContent,
+          pintsLeft: pintsLeft,
+          id: id
+        }
+      });
+    default:
+      return state;
+    }
 };
